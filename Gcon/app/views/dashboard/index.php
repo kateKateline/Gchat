7689a471c1943986f166b1db469ead2665a98633
KateKateline <rizkyramadhan12098@gmail.com>
2025-09-04 13:38:55 +0700

    Delete Gcon directory

--- Gcon/app/views/dashboard/index.php
+++
@@ -1,2 +0,0 @@
-<h1>Dashboard</h1>
-<p>Hanya admin yang bisa melihat halaman ini.</p>
--- Gcon/config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'host' => 'localhost',
-    'user' => 'root',
-    'pass' => '',
-    'name' => 'gcon',
-    'charset' => 'utf8mb4',
-];
